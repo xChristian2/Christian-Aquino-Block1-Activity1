905e6c49a013c77313301cca610db5456f875742
Christian <aquino315532@gmail.com>
2023-12-06 19:48:36 +0800

    media player and background in layout added

--- app/src/main/java/com/aquino/christian/block1/p1/quiz/HelloWorldActivity.kt
+++ app/src/main/java/com/aquino/christian/block1/p1/quiz/HelloWorldActivity.kt
@@ -1,11 +1,25 @@
 package com.aquino.christian.block1.p1.quiz
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class HelloWorldActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello_world)
+
+        // Initialize MediaPlayer
+        mediaPlayer = MediaPlayer.create(this, R.raw.sample)
+        mediaPlayer.start()
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        // Release the MediaPlayer when the activity is destroyed
+        mediaPlayer.release()
     }
 }
