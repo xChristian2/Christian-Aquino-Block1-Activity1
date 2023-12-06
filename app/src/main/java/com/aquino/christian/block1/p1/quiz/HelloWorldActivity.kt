7a6995083f8c519c8fedff2b909e2aaf9e952c68
Christian <aquino315532@gmail.com>
2023-12-06 19:27:43 +0800

    2nd page added

---
+++ app/src/main/java/com/aquino/christian/block1/p1/quiz/HelloWorldActivity.kt
@@ -0,0 +1,11 @@
+package com.aquino.christian.block1.p1.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HelloWorldActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello_world)
+    }
+}
