adb7ca9231b95915bfcd0355e1e081c389b18320
Christian <aquino315532@gmail.com>
2023-12-06 19:27:02 +0800

    added listener to button

--- app/src/main/java/com/aquino/christian/block1/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/aquino/christian/block1/p1/quiz/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.aquino.christian.block1.p1.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.btnClick)
+
+        button.setOnClickListener {
+            intent = Intent(this@MainActivity, HelloWorldActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
